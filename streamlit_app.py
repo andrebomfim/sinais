0c0de4f176a3c86b297ad306d0eb0b85df984d07
Andre <bompires@gmail.com>
2021-03-26 21:50:27 -0300

    Test

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture("http://177.72.3.203:8001/")
+
+while(True):
+    ret, frame = camera.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
